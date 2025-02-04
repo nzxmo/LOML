2560e20cd63f8eff18a8cec1d62c09fad0444c92
JAMES <yrjamesnz@gmail.com>
2025-02-04 09:33:31 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,1 @@
-document.addEventListener("DOMContentLoaded", function () {
-    let button = document.getElementById("show-message");
-    let message = document.getElementById("message");
 
-    button.addEventListener("click", function () {
-        message.classList.toggle("hidden");
-        message.style.opacity = "1";
-    });
-});
