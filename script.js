3003f0bca53d2aa5dfc47dfcd959b5e7646734f5
JAMES <yrjamesnz@gmail.com>
2025-02-04 06:31:02 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const message = document.querySelector(".message");
+    setTimeout(() => {
+        message.style.opacity = "1";
+    }, 500);
+});
